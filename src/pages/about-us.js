00c1ac1a697bac8e95967fb2f9e830e044edbc3f
Xuanyu Wang <Xuanyu.Wang@ibm.com>
2020-09-29 20:22:57 -0400

    init the about us page

---
+++ src/pages/about-us.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AboutUSPageLayout from '../components/about-us/aboutUSPageLayout/about-us-page-layout';
+
+class AboutUSPage extends React.Component {
+	render() {
+		return <AboutUSPageLayout></AboutUSPageLayout>;
+	}
+}
+
+export default AboutUSPage;
