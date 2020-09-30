ece433441cae72df44fb800d74bd60e910c04df0
zewen wang <w.zewen@outlook.com>
2020-09-30 11:26:09 -0400

    add strapi in config and add our team

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,38 @@
 module.exports = {
-	pathPrefix: '/yonghe-online',
-	siteMetadata: {
-		title: 'title',
-		description: 'description',
-		author: 'author',
-	},
-	plugins: [
-		'gatsby-plugin-react-helmet',
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
-				name: 'images',
-				path: `${__dirname}/src/images`,
-			},
-		},
-		'gatsby-transformer-sharp',
-		'gatsby-plugin-sharp',
-		'gatsby-plugin-sass',
-		'gatsby-plugin-manifest'
-		// this (optional) plugin enables Progressive Web App + Offline functionality
-		// To learn more, visit: https://gatsby.dev/offline
-		// `gatsby-plugin-offline`,
-	],
+  pathPrefix: "/yonghe-online",
+  siteMetadata: {
+    title: "title",
+    description: "description",
+    author: "author",
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/src/images`,
+      },
+    },
+
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000, // Default to 100
+        contentTypes: [`our-team`],
+        //If using single types place them in this array.
+        // singleTypes: [`home-page`, `contact`],
+        // Possibility to login with a strapi user, when content types are not publically available (optional).
+      },
+    },
+
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-sass",
+    "gatsby-plugin-manifest",
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 };
