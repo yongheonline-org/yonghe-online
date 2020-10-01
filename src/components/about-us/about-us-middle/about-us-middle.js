d99fadeb805728f26d50ebacf241f8c5ef235877
Xuanyu Wang <xuanyu.wang@outlook.com>
2020-09-30 20:42:17 -0400

    fix imported css (#15)
    Co-authored-by: Xuanyu Wang <Xuanyu.Wang@ibm.com>

--- src/components/about-us/about-us-middle/about-us-middle.js
+++ src/components/about-us/about-us-middle/about-us-middle.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import AboutUsMiddleText from './about-us-middle-text/about-us-middle-text';
+import { Grid, Row, Column } from 'carbon-components-react';
 
 class AboutUSMiddle extends React.Component {
 	render(){
-		return <div className='bx--grid bx--no-gutter'>
-			<div className='bx--row'>
-				<div className='bx--col  bx--no-gutter' style={{ 'background-color': 'rgb(133,35,20'}}>
+		return <Grid condensed>
+			<Row>
+				<Column style={{ 'background-color': 'rgb(133,35,20'}}>
 					<div className="outside bx--aspect-ratio bx--aspect-ratio--2x1">
 						<div className="inside">
 							<AboutUsMiddleText></AboutUsMiddleText>
 						</div>
 					</div>
-				</div>
-				<div className='bx--col  bx--no-gutter' style={{ 'background-color': 'gray'}}>
+				</Column>
+				<Column style={{ 'background-color': 'gray'}}>
 					<div className="outside bx--aspect-ratio bx--aspect-ratio--2x1">
 						<div className="inside"></div>
 					</div>
-				</div>
-			</div>
-		</div>;
+				</Column>
+			</Row>
+		</Grid>;
 	}
 }
 
