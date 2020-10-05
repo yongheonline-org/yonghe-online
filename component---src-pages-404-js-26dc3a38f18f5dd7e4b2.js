e1fbf91868892bd2f1dfacf4b923a9adba3dbc65
zewen wang <w.zewen@outlook.com>
2020-10-04 20:57:05 -0400

    Updates

--- component---src-pages-404-js-26dc3a38f18f5dd7e4b2.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{w2l6:function(e,t,u){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l,n=u("q1tI"),o=(l=n)&&l.__esModule?l:{default:l};t.default=()=>o.default.createElement("h1",null,"404: Not Found")}}]);
-//# sourceMappingURL=component---src-pages-404-js-26dc3a38f18f5dd7e4b2.js.map
