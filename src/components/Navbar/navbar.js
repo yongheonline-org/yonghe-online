c883a388c30964a947e72c22eb815c3f847625a4
Zewen Wang <w.zewen@outlook.com>
2020-10-05 22:30:43 -0400

    fix ourteam and images on about us page, rewrite nav bar (#28)

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,65 +1,41 @@
-
 import React from 'react';
 import logo from './logo.png';
-import { FaAlignRight } from 'react-icons/fa';
 import './navbar.scss';
 import { Link } from 'gatsby';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar,Nav,NavDropdown,Form,Button} from 'react-bootstrap';
 
-class Navbar extends React.Component{
-	constructor(props) {
-		super(props);
-		this.state = {
-			isOpen:false
-		};
-	}
-	handleToggle(){
-		this.setState({isOpen:!this.state.isOpen});
-	}
+
+class NavigationBar extends React.Component{
 	render() {
-		console.log(this.state.isOpen);
-		return <nav className="navbar">
-			<div className="nav-center">
-				<div className="nav-header">
-					<Link to="/">
-						<img src={logo} alt="logo"/>
-					</Link>
-					<button type="button" className="toggle-btn" onClick={this.handleToggle.bind(this)}>
-						<FaAlignRight ></FaAlignRight>
-					</button>
-				</div>				
-				<ul className="page-links nav-links">
-					<li className={typeof window !== 'undefined' && window.location.pathname === '/' ? 'active' : ''}>
-						<Link to='/'>首页</Link>
-					</li>
-					<li className={typeof window !== 'undefined' && window.location.pathname === '/about-us' ? 'active' : ''}>
-						<Link to='/about-us'>关于我们</Link>
-					</li>
-					<li className={typeof window !== 'undefined' && window.location.pathname === '/news' ? 'active' : ''}>
-						<Link to='/news'>新闻中心</Link>
-					</li>
-					<li className={typeof window !== 'undefined' && window.location.pathname === '/fields-info' ? 'active' : ''}>
-						<Link to='/fields-info'>行业资讯</Link>
-					</li>
-					<li className={typeof window !== 'undefined' && window.location.pathname === '/1' ? 'active' : ''}>  
-						<div className="nav-item dropdown">
-							<a className="dropdown-toggle" href="#" role="button" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        旗下平台
-							</a>
-							<div className="dropdown-menu" style={{border:'0',outline:'0'}} aria-labelledby="navbarDropdown">
-								<Link className="dropdown-item" to="/1">雍和珍品</Link>
-								<Link className="dropdown-item" to="/2">雍和真品</Link>
-								<Link className="dropdown-item" to="/3">雍和在线</Link>
-							</div>
-						</div>
-					</li>
-					<li className={typeof window !== 'undefined' && window.location.pathname === '/1' ? 'active' : ''}>
-						<button to='/contact-us' className='nav-links buttonContactUs'>联系我们 </button>
-					</li>
-				</ul>
-			</div>
-		</nav>;
+		return <Navbar bg="white"sticky="top" expand="lg" style={{paddingTop:0, paddingBottom:0}}>
+			<Navbar.Brand as={Link} to="/" className="nav-brand">
+				<img
+					src={logo}
+					className="d-inline-block align-top"
+					style={{marginBottom: '0.375rem'}}
+					alt="logo"
+				/>
+			</Navbar.Brand>			
+			<Navbar.Toggle aria-controls="basic-navbar-nav" className="toggle-btn" />
+			<Navbar.Collapse id="basic-navbar-nav" style={{justifyContent: 'flex-end'}}>
+				<Nav className="nav-links">
+					<Nav.Link className={typeof window !== 'undefined' && window.location.pathname === '/' ? 'active' : ''} as={Link} to="/" >首页</Nav.Link>
+					<Nav.Link className={typeof window !== 'undefined' && (window.location.pathname === '/about-us' || window.location.pathname === '/about-us/') ? 'active' : ''} as={Link} to="/about-us">关于我们</Nav.Link>
+					<Nav.Link className={typeof window !== 'undefined' && (window.location.pathname === '/news' || window.location.pathname === '/news/') ? 'active' : '' } as={Link} to="/news">新闻中心</Nav.Link>
+					<Nav.Link className={typeof window !== 'undefined' && (window.location.pathname === '/fields-info' || window.location.pathname === '/fields-info/')?'active' : ''} as={Link} to="/fields-info">行业资讯</Nav.Link>
+					<NavDropdown className={typeof window !== 'undefined' && window.location.pathname === '/1' ? 'active' : ''}  title="旗下平台" id="basic-nav-dropdown">
+						<NavDropdown.Item className="dropdown-item" as={Link} to="/#1">雍和珍品</NavDropdown.Item>
+						<NavDropdown.Item className="dropdown-item" as={Link} to="/#2">雍和真品</NavDropdown.Item>
+						<NavDropdown.Item className="dropdown-item" as={Link} to="/#3">雍和在线</NavDropdown.Item>
+					</NavDropdown>
+				</Nav>
+				<Form inline>
+					<Button to='/contact-us' className='buttonContactUs'>联系我们</Button>
+				</Form>
+			</Navbar.Collapse>
+		</Navbar>;
+		
 	}
 }
 
-export default Navbar;
+export default NavigationBar;
--- src/components/about-us/about-us-top/about-us-top.js
+++ src/components/about-us/about-us-top/about-us-top.js
@@ -18,6 +18,7 @@ const AboutUSTop = () => {
           presentationHeight
           presentationWidth
           sizes
+          ...GatsbyImageSharpFluid
         }
       }
       testAboutUsTopJson {
