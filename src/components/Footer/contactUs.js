6ca720f26fbbc6bc6e3271fd787d443d7788ccf8
zewen wang <w.zewen@outlook.com>
2020-10-31 18:32:09 -0400

    adjust some styles and add text

---
+++ src/components/Footer/contactUs.js
@@ -0,0 +1,106 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+import React from 'react';
+// import axios from 'axios';
+import emailjs from 'emailjs-com';
+
+
+class ContactForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '', 
+			phone: '', 
+			note: '',
+			disabled: false,
+			emailSent: null,
+			// error: null,
+		};
+	}
+
+	onChange = (event) => {
+		const name = event.target.name;
+		const value = event.target.value;
+		this.setState({
+			[name]: value
+		});
+	}
+
+    onClick = (event) => {
+    	event.preventDefault();
+    	console.log(event.target);
+		
+    	this.setState({
+    		disabled: true
+    	});
+    	emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', event.target, 'YOUR_USER_ID')
+    		.then((result) => {
+    			console.log(result.text);
+    			this.setState({
+    				name:'',
+    				phone:'',
+    				note:'',
+    				disabled:false,
+    				emailSent:true
+    			});
+    		}, (error) => {
+    			console.log(error.text);
+    			this.setState({
+    				disabled:false,
+    				emailSent:false
+    			});
+    		});
+			
+    	// axios.post('http://localhost:3000/api/email', this.state)
+    	// 	.then(res => {
+    	// 		if(res.data.success) {
+    	// 			this.setState({
+    	// 				name: '', 
+    	// 	            phone: '', 
+    	// 	            note: '',
+    	// 				disabled: false,
+    	// 				emailSent: true
+    	// 			});
+    	// 		} else {
+    	// 			this.setState({
+    	// 				disabled: false,
+    	// 				emailSent: false
+    	// 			});
+    	// 		}
+    	// 	})
+    	// 	.catch(err => {
+    	// 		console.log(err);
+    	// 		this.setState({
+    	// 			disabled: false,
+    	// 			emailSent: false
+    	// 		});
+    	// 	});
+    }
+
+    render() {
+    	return (
+    		<div className="card" style={{backgroundColor:'rgb(47,50,59)', border:'1px solid grey'}}>
+    			<form className="formStyle" onSubmit={this.onClick}>
+    				<div className="form-group row justify-content-center" style={{paddingTop:'0.5rem'}}>
+    					<label htmlFor="name" className="col-sm-3" style={{backgroundColor:'rgb(47,50,59)', fontSize:'0.75rem', paddingLeft:'0px'}} >姓名</label>
+    					<input onChange={this.onChange} value={this.state.name} className="col-sm-6 form-control text-white" style={{backgroundColor:'rgb(47,50,59)', fontSize:'0.75rem', height:'1rem', border:'none',  borderBottom: '1px solid grey', borderRadius:'0'}} id="name" name="name" placeholder="请输入姓名" required/>
+    				</div>
+    				<div className="form-group row justify-content-center">
+    					<label className="col-sm-3" htmlFor="phone" style={{backgroundColor:'rgb(47,50,59)', fontSize:'0.75rem', paddingLeft:'0px'}} >电话</label>
+    					<input onChange={this.onChange} value={this.state.phone} className="col-sm-6 form-control text-white" style={{backgroundColor:'rgb(47,50,59)', fontSize:'0.75rem', height:'1rem', border:'none', borderBottom:'1px solid grey',borderRadius:'0'}} id="phone" name="phone" placeholder="请输入电话" required/>
+    				</div>
+    				<div className="form-group row justify-content-center ">
+    					<label className="col-sm-3" htmlFor="note" style={{backgroundColor:'rgb(47,50,59)', fontSize:'0.75rem', paddingLeft:'0px'}} >留言</label>
+    					<input onChange={this.onChange} value={this.state.note} className="col-sm-6 form-control text-white" id="note" name="note" style={{backgroundColor:'rgb(47,50,59)', fontSize:'0.75rem', height:'1rem',border:'none', borderRadius:'0',borderBottom:'1px solid grey'}} rows="1" placeholder="请输入您的留言" required></input>
+    				</div>
+    				<div className="form-group row justify-content-center">
+    					<button type="submit" disabled={this.state.disabled}  className="btn btn-primary mb-2 text-dark bg-light" style={{borderRadius:'20px', fontSize:'0.75rem', marginBottom:'0rem',padding: '0.1rem 1.25rem' }} >发送</button>
+    					{this.state.emailSent === true && <p className="d-inline success-msg">发送成功</p>}
+    					{this.state.emailSent === false && <p className="d-inline err-msg">发送失败</p>}
+    				</div>
+    			</form>
+    		</div>
+    	);
+    }
+}
+
+export default ContactForm;
--- src/components/homePageLayout/MoreButton/MoreButton.js
+++ src/components/homePageLayout/MoreButton/MoreButton.js
@@ -5,8 +5,8 @@ import { Link } from 'gatsby';
 
 
 const MoreButton = ({className, path}) => {
-	return<Button variant="primary" className={className} style={{backgroundColor:'#902413',border: 'none', fontSize:'17px', borderRadius:'25px', padding:'0.4rem 2.6rem 0.4rem 2rem',  fontWeight:'500'}}>
-		<Link to={path} style={{textDecoration: 'none',color:'white'}}>了解更多<MdKeyboardArrowRight size={25} style={{ height: 25, width: 25, margin : 'auto',position:'absolute'}}/>
+	return<Button variant="primary" className={className} style={{backgroundColor:'#902413',border: 'none', fontSize:'1.2vw', borderRadius:'1.7vw', padding:'0.6vw 2.8vw 0.6vw 2.2vw',  fontWeight:'500'}}>
+		<Link to={path} style={{textDecoration: 'none',color:'white'}}>了解更多<MdKeyboardArrowRight style={{ height: '1.7vw', width: '1.7vw', margin : 'auto',position:'absolute'}}/>
 		</Link></Button>;
 };
 export default MoreButton;
