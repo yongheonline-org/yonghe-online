11c942999e2430144a876b450d637eee92a4a6c6
Xuanyu Wang <Xuanyu.Wang@ibm.com>
2020-11-01 19:33:16 -0500

    update field info top

--- src/components/fieldsInfo/fields-info-top/fields-info-top.js
+++ src/components/fieldsInfo/fields-info-top/fields-info-top.js
@@ -1,49 +1,61 @@
 import React from 'react';
-import TwoColumns from '../../common/two-columns/two-columns';
 import { useStaticQuery, graphql } from 'gatsby';
-import Img from 'gatsby-image';
 
 import './fields-info-top.scss';
 
 const leftSide = (props) => {
-	const { fluid } = props;
-	return <Img fluid={fluid} style={{ height: '100%', width: '100%'}}/>;
+	const { brannerLeftURL } = props;
+	return <div className='banner-image-container' >
+		<img src={brannerLeftURL}></img>
+	</div>;
 };
 const rightSide = (props) => {
-	const { header_title: title, header_content: content} = props;
-	return <div className='' style={{ color: 'white', width: '70%', maxHeight: '70%', height: 'fit-content', display: 'flex', flexDirection: 'column' }}>
-		<div className='' style={{ fontSize: '32px', fontWeight: 'bold', paddingBottom: '12px'}}>
-			{title}
+	const { pageTitle, pageSubtitle, pageTitleEnglish } = props;
+	return <div className='banner-text-container'>
+		<div className='banner-text-left'>
+			<div className='banner-title' >
+				{pageTitle}
+			</div>
+			<div className='banner-subtitle'>
+				{pageSubtitle}
+			</div>
 		</div>
-		<div className='' style={{ fontSize: '14px', overflow: 'hidden'}}>
-			{content}
+		<div className='banner-title-english'>
+			{pageTitleEnglish}
 		</div>
 	</div>;
 };
 const FieldsInfoTop = () => {
 	const data = useStaticQuery(graphql`
     {
-      imageSharp(fluid: {originalName: {eq: "example1.jpg"}}) {
-        fluid {
-          base64
-          tracedSVG
-          srcWebp
-          srcSetWebp
-          originalImg
-          originalName
+      prismicFieldinfopage {
+        data {
+          banner_left {
+            url
+          }
+          page_title
+          page_subtitle
+          page_title_english
         }
       }
-      testFieldsInfoJson {
-        header_title
-        header_content
-      }
-    }
-  `);
-	return <TwoColumns className='fields-info-top'
-		leftColChildren={leftSide({ fluid: data.imageSharp.fluid })}
-		rightColChildren={rightSide(data.testFieldsInfoJson)}
-		aspectRatio={{ width: 2, height: 1 }}
-	/>;
+	}`);
+	const pageData = data.prismicFieldinfopage.data;
+	const brannerLeftURL = pageData.banner_left.url;
+	const pageTitle = pageData.page_title;
+	const pageSubtitle = pageData.page_subtitle;
+	const pageTitleEnglish = pageData.page_title_english;
+	return <div className='fields-info-top'>
+		<div className='left-container'>
+			{leftSide({ brannerLeftURL })}
+		</div>
+		<div className='right-container'>
+			{rightSide({
+				pageTitle,
+				pageSubtitle,
+				pageTitleEnglish
+			})}
+		</div>
+	</div>;
 };
 
 export default FieldsInfoTop;
