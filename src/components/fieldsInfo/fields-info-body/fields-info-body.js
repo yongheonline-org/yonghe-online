47e5503fffcb00e1f9516ff82492119f383b9cfb
Xuanyu Wang <Xuanyu.Wang@ibm.com>
2020-11-02 00:05:56 -0500

    fied info page

--- src/components/fieldsInfo/fields-info-body/fields-info-body.js
+++ src/components/fieldsInfo/fields-info-body/fields-info-body.js
@@ -4,6 +4,12 @@ import {useStaticQuery, graphql} from 'gatsby';
 
 import './fields-info-body.scss';
 
+const STYLES = {
+	DEFAULT: 0,
+	ALTERNATE: 1,
+	SPAN: 2
+};
+
 const FieldInfoCard = (infoBlockData, style, imgURL) => {
 	const styleName = 'style-' + style;
 	const card = <div className='info-card'>
@@ -24,6 +30,18 @@ const FieldInfoCard = (infoBlockData, style, imgURL) => {
 		cardContent = <React.Fragment>
 			{image}
 			{card}
+		</React.Fragment>;
+	}else if(style === 2){
+		cardContent = <React.Fragment>
+			{image}
+			<div className='info-card'>
+				<div className='card-title-section'>
+					<div className='info-title'>{infoBlockData.info_title}</div>
+					<div className='info-date'>{infoBlockData.info_date}</div>
+					<div className='left-quote'>“</div>
+				</div>
+				<div className='info-content'>{infoBlockData.info_content}</div>
+			</div>
 		</React.Fragment>;
 	}
 	return <div className={`field-info-card ${styleName}`}>
@@ -69,18 +87,28 @@ const FieldInfoBody = () => {
 	const background_02 = prismicFieldinfopage.data.background_02;
 	const neededInfoBlockIDs = prismicFieldinfopage.data.field_info_group.map((item) => { return item.field_info_block.id; });
 
-	const infoCards = neededInfoBlockIDs.map((id, index) => {
-		const infoBlockData = allInfoBlocks.filter((block) => {
+	const infoBlocks = neededInfoBlockIDs.map((id) => {
+		return allInfoBlocks.filter((block) => {
 			return block.node.prismicId === id;
 		})[0].node.data;
-		const style = index % 2;
+	});
+	let lastInfoCard = null;
+	if (infoBlocks.length % 2 === 1){
+		const lastInfoBlock = infoBlocks.pop();
+		lastInfoCard = FieldInfoCard(lastInfoBlock, STYLES.SPAN, background_01.url);
+	}
+
+	const styleMap = [STYLES.DEFAULT, STYLES.ALTERNATE, STYLES.ALTERNATE, STYLES.DEFAULT];
+	const infoCards = infoBlocks.map((infoBlock, index) => {
+		const style = styleMap[index % styleMap.length];
 		const decorationImg = style === 0 ? background_02.url : background_01.url;
-		return FieldInfoCard(infoBlockData, style, decorationImg);
+		return FieldInfoCard(infoBlock, style, decorationImg);
 	});
 
 	return <Container className='field-info-body-container'>
 		<div className='field-info-columns'>
 			{infoCards}
+			{lastInfoCard}
 		</div>
 	</Container>;
 };
