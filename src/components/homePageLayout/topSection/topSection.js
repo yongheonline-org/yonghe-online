8e48f378a1fe38a807ab59f7de54b80a3e8806b5
Xuanyu Wang <xuanyu.wang@outlook.com>
2020-11-12 22:12:14 -0500

    Fix the build issue (#53)

--- src/components/homePageLayout/topSection/topSection.js
+++ src/components/homePageLayout/topSection/topSection.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
